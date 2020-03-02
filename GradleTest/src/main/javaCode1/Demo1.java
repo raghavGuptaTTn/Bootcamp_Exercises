43b6a2c4f5339af7c4258cee910672cbd8e506af
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-03-02 16:49:38 +0530

    gradle exercise submitted

---
+++ GradleTest/src/main/javaCode1/Demo1.java
@@ -0,0 +1,11 @@
+
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+        Demo2 dm2 = new Demo2();
+        System.out.println("i am from demo1 calling demo2");
+        dm2.show();
+    }
+}
---
+++ GradleTest/src/main/javaCode2/Demo2.java
@@ -0,0 +1,8 @@
+
+
+public class Demo2 {
+
+    void show(){
+        System.out.println("i am demo2 class ");
+    }
+}
